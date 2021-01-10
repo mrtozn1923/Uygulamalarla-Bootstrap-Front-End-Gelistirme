e64e91c64a5862ce939666080fc70d96bb82daaf
mrtozn1923 <mrtozn1923@gmail.com>
2021-01-10 20:33:37 +0300

    Lesson31 added

---
+++ 31-Collapse/js/app.js
@@ -0,0 +1,31 @@
+// $(document).ready(function(){
+//     $(".btn-primary").click(function(){
+//         $("#box").collapse('toggle');
+//     });
+//     $(".btn-success").click(function(){
+//         $("#box").collapse('show');
+//     });
+//     $(".btn-danger").click(function(){
+//         $("#box").collapse('hide');
+//     });
+
+// //show.bs.collapse ->gösterme işlemi başladığında gerçekleşir
+// //shown.bs.collapse ->gösterme işlemi tamamlandığında gerçekleşir
+// //hide.bs.collapse ->gizle işlemi başladığında gerçekleşir
+// //hidden.bs.collapse ->gizle işlemi tamamlandığında gerçekleşir
+//     $("#box").on('show.bs.collapse',function(){
+//         alert("gösterme işlemi başladı");
+//     });
+//     $("#box").on('shown.bs.collapse',function(){
+//         alert("gösterme işlemi tamamlandı");
+//     });
+//     $("#box").on('hide.bs.collapse',function(){
+//         alert("gizleme işlemi başladı");
+//     });
+//     $("#box").on('hidden.bs.collapse',function(){
+//         alert("gizleme işlemi tamamlandı");
+//     });
+
+// });
+
+//---------------------------------------------
