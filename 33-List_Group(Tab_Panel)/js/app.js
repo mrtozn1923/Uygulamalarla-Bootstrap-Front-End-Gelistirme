0bf69b807ea5f2e60508d22da4e13059604a5275
mrtozn1923 <mrtozn1923@gmail.com>
2021-01-11 15:40:52 +0300

    Lesson33 added

---
+++ 33-List_Group(Tab_Panel)/js/app.js
@@ -0,0 +1,20 @@
+$(function() {
+    // $('#list-tab a').on('click', function() {
+    //     $(this).tab('show');
+    // });
+    // $('#list-tab a[href="#list-profile"]').on('click',function(){
+    //     $(this).tab('show');
+    // });
+    // $('#list-tab a:nth-child(3)').on('click',function(){
+    //     $(this).tab('show');
+    // });
+
+    //hide.bs.tab
+    //show.bs.tab
+    //hidden.bs.tab
+    //shown.bs.tab
+    // $('#list-tab a').on('hide.bs.tab', function(e) {
+    //     console.log(e.target);
+    //     console.log(e.relatedTarget);
+    // });
+});
